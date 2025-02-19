17b669c287f2739d798d2c027cae93665bc5a4ab
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-19 16:34:57 +0100

    search event and test - integration test

--- src/model/repositories/eventos_repository.py
+++ src/model/repositories/eventos_repository.py
@@ -9,6 +9,19 @@ class EventosRepository:
             db.session.add(new_event) #adiciona o novo evento na sessao
             db.session.commit() #comita o novo evento
          except Exception as exception:
-            db.session.rollback() #volta ao estado anterior se der algum erro
+            db.session.rollback() #volta ao estado anterior se der algum erro, é recomendável em caso de inserção, alteração ou deleção no banco de dados
             raise exception
-            
+         
+    #busca
+         
+   def select_event(self, event_name:str) -> Eventos:
+      with DBConnetionHandler() as db:
+         data  = (
+            db.session
+            .query(Eventos)
+            .filter(Eventos.nome == event_name)
+            .one_or_none()
+         )
+         return data
+
+
--- src/model/repositories/eventos_repository_test.py
+++ src/model/repositories/eventos_repository_test.py
@@ -1,7 +1,17 @@
+import pytest
 from .eventos_repository import EventosRepository
 
+@pytest.mark.skip("Insert in DB")
 def test_insert_eventos():
    event_name = "eventoTeste2"
    event_repo = EventosRepository()
 
    event_repo.insert(event_name)
+
+def test_select_event():
+   event_name = "eventoTeste2"
+   event_repo = EventosRepository()
+
+   event = event_repo.select_event(event_name)
+   print(event)
+   print(event.nome)
