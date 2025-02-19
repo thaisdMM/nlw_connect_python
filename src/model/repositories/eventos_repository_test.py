f4bcfca8f4f707762e819b2e8da73c652587d70a
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-19 16:39:23 +0100

    Update eventos_repository_test.py

--- src/model/repositories/eventos_repository_test.py
+++ src/model/repositories/eventos_repository_test.py
@@ -2,16 +2,15 @@ import pytest
 from .eventos_repository import EventosRepository
 
 @pytest.mark.skip("Insert in DB")
-def test_insert_eventos():
+def test_insert_eventos(): #teste de integração
    event_name = "eventoTeste2"
    event_repo = EventosRepository()
 
    event_repo.insert(event_name)
 
-def test_select_event():
+@pytest.mark.skip("Select in DB")
+def test_select_event(): #teste de integração
    event_name = "eventoTeste2"
    event_repo = EventosRepository()
 
    event = event_repo.select_event(event_name)
-   print(event)
-   print(event.nome)
