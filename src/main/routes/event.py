25078a424eceaba57e9ccda4b6b08df0a6ba79c4
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 11:48:58 +0100

    created events validator

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -6,9 +6,12 @@ event_route_bp = Blueprint("event_route", __name__)
 
 from src.http_types.http_response import HttpResponse 
 from src.http_types.http_request import HttpRequest
+from src.validators.events_creator_validator import events_creator_validator
 
 @event_route_bp.route("/event", methods=["POST"]) #Rota de criação de eventos
 def create_new_event():
+
+   events_creator_validator(request)
 
    htpp_request = HttpRequest(body=request.json) #tira o body da requisição. Coleta só que é necessário da framework
 
---
+++ src/validators/events_creator_validator.py
@@ -0,0 +1,18 @@
+from cerberus import Validator
+
+def events_creator_validator(request: any): #vai ser o mesmo request do flask
+  body_validator = Validator({
+      "data": {
+         "type": "dict",
+         "schema" : {
+           "name": {"type": "string", "required": True, "empty": False} #cerberus ajuda a não ter que fazer if/else
+      
+      }
+      }
+      })
+  
+  response = body_validator.validate(request.json) #esse body_validator está validando o body do flask
+
+  if response is False:
+    print(body_validator.errors)
+
