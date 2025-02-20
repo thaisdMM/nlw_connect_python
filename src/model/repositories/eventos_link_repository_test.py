c58878bfc8f1e11a491c568fb55d617bd7aaaaaf
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 19:05:46 +0100

    created eventos link py test

---
+++ src/model/repositories/eventos_link_repository_test.py
@@ -0,0 +1,9 @@
+from .eventos_link_repository import EventosLinkRepository
+
+def test_insert_eventos_link():
+   event_id = 12
+   subs_id = 8
+   event_link_repo = EventosLinkRepository()
+
+   event_link_repo.insert(event_id, subs_id)
+
