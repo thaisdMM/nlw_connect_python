459f3d5fc1f0dac0262562c98cb38d909a1036e0
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 14:32:54 +0100

    events creator update

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -4,17 +4,24 @@ from flask import Blueprint, jsonify, request
 
 event_route_bp = Blueprint("event_route", __name__)
 
+from src.validators.events_creator_validator import events_creator_validator
+
 from src.http_types.http_response import HttpResponse 
 from src.http_types.http_request import HttpRequest
-from src.validators.events_creator_validator import events_creator_validator
+
+from src.controllers.events.events_creator import EventsCreator
+from src.model.repositories.eventos_repository import EventosRepository
 
 @event_route_bp.route("/event", methods=["POST"]) #Rota de criação de eventos
 def create_new_event():
 
-   events_creator_validator(request)
-
+   events_creator_validator(request) #validação
    htpp_request = HttpRequest(body=request.json) #tira o body da requisição. Coleta só que é necessário da framework
 
+   eventos_repo = EventosRepository()
+   events_creator = EventsCreator(eventos_repo)
+
+   response = events_creator.create(htpp_request)
 
    http_response = HttpResponse(body={"estou": "aqui" }, status_code=201) #lógica
 
