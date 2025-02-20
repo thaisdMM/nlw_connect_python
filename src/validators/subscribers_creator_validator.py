f39a44ddd20d24ff0dfa8bb372bb72ec969d6c61
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 12:03:50 +0100

    Create subscribers_creator_validator.py

---
+++ src/validators/subscribers_creator_validator.py
@@ -0,0 +1,22 @@
+from cerberus import Validator
+
+def subscribers_creator_validator(request: any): #vai ser o mesmo request do flask
+
+#cerberus ajuda a construir validadores sem ter que fazer if/else 
+  body_validator = Validator({
+      "data": {
+         "type": "dict",
+         "schema" : {
+           "name": {"type": "string", "required": True, "empty": False}, 
+           "email": {"type": "string", "required": True, "empty": False},
+           "link": {"type": "string",  "required": False, "empty": False},
+            "evento_id": {"type": "integer",  "required": True, "empty": False},
+      
+      }
+      }
+      })
+  
+  response = body_validator.validate(request.json) #esse body_validator está validando o body do flask
+
+  if response is False:
+    print(body_validator.errors)
