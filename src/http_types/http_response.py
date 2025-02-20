aa55adb35156fb20a2e7e49007e5232ee54e6419
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 10:49:19 +0100

    created http response

---
+++ src/http_types/http_response.py
@@ -0,0 +1,6 @@
+#padronizar
+
+class HttpResponse:
+   def __init__(self, body: dict, status_code: int) -> None:
+      self.body = body
+      self.status_code = status_code
--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -4,6 +4,11 @@ from flask import Blueprint, jsonify
 
 event_route_bp = Blueprint("event_route", __name__)
 
+from src.http_types.http_response import HttpResponse 
+
 @event_route_bp.route("/event", methods=["POST"]) #Rota de criação de eventos
 def create_new_event():
-   return jsonify({"estou": "aqui" }), 201
+
+   http_response = HttpResponse(body={"estou": "aqui" }, status_code=201) #lógica
+
+   return jsonify(http_response.body), http_response.status_code #sempre vai querer esse retorno em todas as rotas
