f29113f70634e90750fa680a7ce9599ccc51355f
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 14:41:45 +0100

    Update events_creator_validator.py

--- src/validators/events_creator_validator.py
+++ src/validators/events_creator_validator.py
@@ -5,14 +5,13 @@ def events_creator_validator(request: any): #vai ser o mesmo request do flask
       "data": {
          "type": "dict",
          "schema" : {
-           "name": {"type": "string", "required": True, "empty": False} #cerberus ajuda a não ter que fazer if/else
-      
+           "name": {"type": "string", "required": True, "empty": False} #cerberus ajuda a não ter que fazer if/else 
+         }
       }
-      }
-      })
+   })
   
   response = body_validator.validate(request.json) #esse body_validator está validando o body do flask
 
   if response is False:
-    print(body_validator.errors)
+    raise Exception(body_validator.errors)
 
