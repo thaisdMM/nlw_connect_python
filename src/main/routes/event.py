81d24b25ebeb97b5ea93f65d1ce5e6abe5a20c48
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-20 11:05:12 +0100

    created http request and changed event.py

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,13 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 #agregador de rotas de eventos, assim se tiver mais de uma rota aqui todas rodariam no server.py
 
 event_route_bp = Blueprint("event_route", __name__)
 
 from src.http_types.http_response import HttpResponse 
+from src.http_types.http_request import HttpRequest
 
 @event_route_bp.route("/event", methods=["POST"]) #Rota de criação de eventos
 def create_new_event():
+
+   htpp_request = HttpRequest(body=request.json) #tira o body da requisição. Coleta só que é necessário da framework
+   print(htpp_request.body)
 
    http_response = HttpResponse(body={"estou": "aqui" }, status_code=201) #lógica
 
